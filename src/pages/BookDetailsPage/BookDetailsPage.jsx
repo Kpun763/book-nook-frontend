4584e6eccf4e0dce802a7ae34ed81687b108ff7d
Kyle Washington <kmw1993@msn.com>
2023-09-21 14:53:38 -0400

    Added the useEffect to fetch book data that is now showing in the console log for the bookdetailspage.jsx

--- src/pages/BookDetailsPage/BookDetailsPage.jsx
+++ src/pages/BookDetailsPage/BookDetailsPage.jsx
@@ -1,8 +1,25 @@
 import Book from "./Book";
 import React from "react";
-
+import axios from "axios";
+import { useEffect } from "react";
 
 const BookDetailPage = ({ bookId }) => {
+  const fetchBook = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=AIzaSyBZmkIqDVpw-NRuvGOjYm2TGGjEp-M1Bjo"
+      );
+      console.log(response.data);
+      console.log(response);
+    } catch (error) {
+      console.warn("Error in fetchBook request: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBook();
+  }, []);
+
   return (
     <div>
       <Book />
